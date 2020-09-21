65bc9378aa761774d1d9ba29f4f94f0a226d5d12
derilion <mbier@gmx.net>
2020-09-21 21:50:00 +0200

    updated setup.py with dependencies

--- icarus_base/setup.py
+++ icarus_base/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-with open("README.md", 'r') as f:
+with open("../README.md", 'r') as f:
     long_description = f.read()
 
 setup(
@@ -12,8 +12,19 @@ setup(
    author='derilion',
    author_email='foo@mail.com',
    url="http://www.github.com/derilion/icarus/",
-   packages=['src'],
-   install_requires=['bar', 'greek'],  # todo: need to be updated to actual dependencies
+   packages=['src', 'skills', 'src.Clients', 'src.Persistence', 'src.SkillManagement'],
+   install_requires=['pyaudio',
+                     'pyttsx3',
+                     'gtts',
+                     'SpeechRecognition',
+                     'playsound',
+                     'pvporcupine',
+                     # from here on skill dependencies are listed
+                     'python-telegram-bot',
+                     'wikipedia',
+                     'caldav',
+                     'icalendar'
+                     ],  # todo: need to be updated to actual dependencies
    scripts=[
             'logger.py',
             'main.py',
