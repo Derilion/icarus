fa305c934a51efc512c0f5c2493d91ee92eb5d4d
Derilion <MBier@gmx.net>
2020-10-18 23:50:57 +0200

    Skeleton implementation telegram voice message support

--- icarus/clients/telegramclient.py
+++ icarus/clients/telegramclient.py
@@ -3,7 +3,9 @@ from icarus.clients.superclient import SuperClient
 from icarus.logging import icarus_logger
 
 from telegram.ext import Updater, Filters, MessageHandler
-from telegram.error import InvalidToken
+from telegram.error import InvalidToken, TelegramError
+
+import speech_recognition as sr
 BOT_TOKEN = ""
 
 
@@ -19,12 +21,14 @@ class TelegramClient(SuperClient):
             self.updater = Updater(token=self.persistence.get_config('Telegram', 'token'), use_context=True)
             self.dispatcher = self.updater.dispatcher
         except InvalidToken:
-            # todo: add logging
+            icarus_logger.error("Telegram Token invalid")
             return
 
         # append handler
-        echo_handler = MessageHandler(Filters.text, self.incoming_message_handler)
-        self.dispatcher.add_handler(echo_handler)
+        text_handler = MessageHandler(Filters.text, self.incoming_message_handler)
+        voice_handler = MessageHandler(Filters.voice, self.incoming_voice_handler)
+        self.dispatcher.add_handler(text_handler)
+        self.dispatcher.add_handler(voice_handler)
 
         # start responding
         self.updater.start_polling()
@@ -32,6 +36,27 @@ class TelegramClient(SuperClient):
     def incoming_message_handler(self, sender, more):
         # old implementation: self._queue_new_message(context.message.text, {self.CONTEXT_IDENT: context})
         self._queue_new_message(sender.message.text, {self.CONTEXT_IDENT: sender})
+
+    def incoming_voice_handler(self, sender, more):
+        # process audio
+        try:
+            link = sender.message.voice.get_file()
+            link.download("telegram.opus")
+        except TelegramError:
+            print("Could not load file")
+        processed_text = ""
+
+        # todo: does not work, must be wav or flac
+        try:
+            r = sr.Recognizer()
+            with sr.AudioFile("telegram.opus") as source:
+                processed_text = r.recognize_google(source)
+
+            print(f"recognized: {processed_text}")
+            self._queue_new_message(processed_text, {self.CONTEXT_IDENT: sender})
+        except:
+            self.send("I do not support voice messages - yet", {self.CONTEXT_IDENT: sender})
+
 
     def send(self, message: str, client_attr):
         context = client_attr[self.CONTEXT_IDENT]
